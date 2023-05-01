0857a3e79da6249d3d2e50b06a6f205cecce8da3
Hisona229 <127940984+Hisona229@users.noreply.github.com>
2023-05-01 10:52:05 +0300

    Initial commit

---
+++ PersonalWebSite.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <script
+        src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Personal WebSite</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght
+@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <style>
+            p {color:darkslategrey;}
+            .MyImage{
+                width: 15px;
+            }
+            </style>
+</head>
+<body>
+<div class="box">
+    <img src="img/52218902-software-developer-coder-at-workcomic-book-pop-art-retro-style-vector-illustration-softwareenginee.webp" alt="" class="box-img">
+    <h1>Harry Coder</h1>
+    <h5>Web Devloper - Web Designer</h5>
+
+    <p>
+        Веб-розробник - це програміст, який спеціалізується на розробці додатків <br>
+        для всесвітньої павутини,або додатків, які виконуються за допомогою HTTP <br>
+        з веб-сервера  на веб-браузер. Гаррі Кодер стиляга на бархатних тягах.
+    </p>
+    <ul>
+        <li><a href="https://twitter.com/?lang=uk"><i class="fa fa-twitter-square" ariahidden="true"></i></a></li>
+        <li><a href="https://www.youtube.com/watch?v=YLo00hieCJ8"><i class="fa fa-youtube-square" ariahidden="true"></i></a></li>
+        <li><a href="https://www.google.com.ua/?hl=uk"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.youtube.com/watch?v=YLo00hieCJ8"><img src="image/youtube.png" class="MyImage"></a></li>
+
+
+    </ul>
+
+</div>
+</body>
+</html>
